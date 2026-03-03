adbf8cc88a3ce0fee3a56e4cb365e849f6508a00
LuisAngel119 <119964329+LuisAngel119@users.noreply.github.com>
2026-03-03 15:49:59 -0600

    Examen1ºparcial

--- client/src/core/services/busy-service.spec.ts
+++ client/src/core/services/busy-service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MembersService } from './members-service';
+import { BusyService } from './busy-service';
 
-describe('MembersService', () => {
-  let service: MembersService;
+describe('BusyService', () => {
+  let service: BusyService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MembersService);
+    service = TestBed.inject(BusyService);
   });
 
   it('should be created', () => {
