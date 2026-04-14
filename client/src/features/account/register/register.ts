56f5e867445d8a6bd779671ab2ddbb4f11942e8a
LuisAngel119 <119964329+LuisAngel119@users.noreply.github.com>
2026-04-13 19:55:37 -0600

    Parcial05 - Add likes pagination to SPA

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,51 +1,87 @@
-import { Component, inject, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';import { AccountService } from '../../../core/services/account-service';
+import { Component, inject, output, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AccountService } from '../../../core/services/account-service';
 import { RegisterCreds } from '../../../types/user';
 import { JsonPipe } from '@angular/common';
+import { TextInput } from "../../../shared/text-input/text-input";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register implements OnInit {
+export class Register {
   private accountService = inject(AccountService);
-
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
   protected creds = {} as RegisterCreds;
-  protected registerForm: FormGroup = new FormGroup({});
+  protected credentialsForm: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
   cancelRegister = output<boolean>();
 
-  private readonly EMAIL_REGEX = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+  constructor() {
+    this.credentialsForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      displayName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
 
-  ngOnInit(): void {
-    this.initializeForm();
+    this.profileForm = this.fb.group({
+      gender: ['', Validators.required],
+      birthDay: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+    });
+
+    this.credentialsForm.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
+    });
   }
 
-  initializeForm() {
-    this.registerForm = new FormGroup({
-      email: new FormControl('',
-        [Validators.required, Validators.pattern(this.EMAIL_REGEX)]),
-      displayName: new FormControl('',
-        [Validators.required]),
-      password: new FormControl('',
-        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('',
-        [Validators.required])
-    })
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+      if (!parent) return null;
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : { passwordMismatch: true };
+    }
+  }
+
+  nextStep() {
+    if (this.credentialsForm.valid) {
+      this.currentStep.update(prevStep => prevStep + 1);
+    }
+  }
+
+  prevStep() {
+    this.currentStep.update(prevStep => prevStep - 1);
+  }
+
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split('T')[0];
   }
 
   register(): void {
-    console.group("REGISTER");
-    console.log(this.registerForm.value);
-    console.groupEnd();
-    // this.accountService.register(this.creds).subscribe({
-    //   next: response => {
-    //     console.log(response);
-    //     this.cancel();
-    //   },
-    //   error: error => console.log(error)
-    // });
+    if (this.credentialsForm.valid && this.profileForm.valid) {
+      const formData = { ...this.credentialsForm.value, ...this.profileForm.value };
+
+      this.accountService.register(formData).subscribe({
+        next: () => {
+          this.router.navigateByUrl('/members');
+        },
+        error: error => {
+          this.validationErrors.set(error);
+          console.log(error);
+        }
+      });
+    }
   }
 
   cancel(): void {
